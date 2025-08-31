e348c06b59407f419050d2e3508b0e3cbf6a9626
Ziad Gad <ziadamrjad@gmail.com>
2025-08-31 06:51:45 +0300

    feat: implement welcome message for main route

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,9 @@ const songRouter = require('./songRoutes');
 const userRouter = require('./userRoutes');
 
 const mountRoutes = (app) => {
+  app.use('/', (req, res, next) => {
+    res.status(200).send('Welcome to Spotify Clone API 🤗');
+  });
   app.use('/api/v1/users', userRouter);
   app.use('/api/v1/artists', artistRouter);
   app.use('/api/v1/albums', albumRouter);
